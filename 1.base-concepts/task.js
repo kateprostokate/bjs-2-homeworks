e48ccac72dae26b78c6c65d5083da4735e45b80a
Neto test <borisovaed2014@yandex.ru>
2025-07-11 15:46:38 +0300

    выполнила задание номер два

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,3 +19,30 @@ function solveEquation(a, b, c) {
     }
     return arr;
 }
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+    // параметры в числа 
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+    countMonths = Number(countMonths);
+    // проверка на некорректные значения
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+        return false;
+    }
+    // тело кредита
+    const S = amount - contribution;
+    if (S <= 0) {
+        return 0;
+    }
+    // месячная процентная ставка
+    const P = percent / 100 / 12;
+    // формула ежемесячного платежа
+    const monthlyPayment = S * (P + (P / ((Math.pow(1 + P, countMonths) - 1))));
+    // общая сумма выплат
+    const totalPayment = monthlyPayment * countMonths;
+    // округление до двух знаков после запятой и возврат числа
+    return +totalPayment.toFixed(2);
+}
+
+
