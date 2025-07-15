28c3a9805d73d9a765cf29f8735abf24e3083163
Neto test <borisovaed2014@yandex.ru>
2025-07-15 15:20:04 +0300

    Реализована функция getArrayParams для задачи 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,20 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let number = arr[i];
+    if (number < min) min = number;
+    if (number > max) max = number;
+    sum += number;
+  }
+
+  let avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
+
 
 function summElementsWorker(...arr) {
 
