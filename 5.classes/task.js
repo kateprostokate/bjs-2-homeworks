bca7f0d1edde8b3339d0ea17e66aaa5dcd0cb582
Neto test <borisovaed2014@yandex.ru>
2025-07-26 19:29:26 +0300

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,3 +1,4 @@
+//1
 class PrintEditionItem {
   constructor(name, releaseDate, pagesCount) {
     this.name = name;
@@ -8,16 +9,16 @@ class PrintEditionItem {
   }
 
   fix() {
-    this.state = this._state * 1.5;
+    this.state = this.state * 1.5;
   }
 
-  set state(newState) {
-    if (newState < 0) {
+  set state(value) {
+    if (value < 0) {
       this._state = 0;
-    } else if (newState > 100) {
+    } else if (value > 100) {
       this._state = 100;
     } else {
-      this._state = newState;
+      this._state = value;
     }
   }
 
@@ -59,5 +60,86 @@ class DetectiveBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
     super(author, name, releaseDate, pagesCount);
     this.type = "detective";
+  }
+}
+
+//2
+class Library {
+  constructor(name) {
+    this.name = name;     // Название библиотеки
+    this.books = [];      // Хранилище книг (изначально пустой массив)
+  }
+
+  addBook(book) {
+    // Добавляем книгу только если её состояние > 30
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    // Ищем книгу по ключу (например, "name") и значению
+    for (const book of this.books) {
+      if (book[type] === value) {
+        return book;
+      }
+    }
+    return null;
+  }
+
+  giveBookByName(bookName) {
+    // Ищем книгу по названию
+    const index = this.books.findIndex((book) => book.name === bookName);
+
+    if (index !== -1) {
+      // Удаляем книгу из массива и возвращаем её
+      const book = this.books.splice(index, 1)[0];
+      return book;
+    }
+
+    return null; // Если книга не найдена
+  }
+}
+
+//3
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {}; // объект вида { "математика": [5, 4], "физика": [3] }
+  }
+
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      console.warn(`Оценка ${mark} не добавлена: должна быть от 2 до 5`);
+      return;
+    }
+
+    if (!this.marks[subject]) {
+      this.marks[subject] = [];
+    }
+
+    this.marks[subject].push(mark);
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks[subject] || this.marks[subject].length === 0) {
+      return 0;
+    }
+
+    const sum = this.marks[subject].reduce((acc, m) => acc + m, 0);
+    return sum / this.marks[subject].length;
+  }
+
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    if (subjects.length === 0) {
+      return 0;
+    }
+
+    const total = subjects.reduce((acc, subj) => {
+      return acc + this.getAverageBySubject(subj);
+    }, 0);
+
+    return total / subjects.length;
   }
 }
